675fb63ad6564c474e9fc59c116ffa4d72444f04
sosow0212 <sosow0212@naver.com>
2022-12-01 13:41:54 +0900

    feat : (Car) 자동차들의 작동 기능 구현

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,20 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public void move() {
+        int value = Randoms.pickNumberInRange(0, 9);
+        if (value >= 4) {
+            this.position++;
+        }
+    }
+}
