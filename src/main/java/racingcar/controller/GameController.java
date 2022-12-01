77a5b0c9dcdf31d5d02b6284181f5a451c210aae
sosow0212 <sosow0212@naver.com>
2022-12-01 14:22:15 +0900

    feat : (GameController) 게임의 전체 흐름을 담당해주는 controlelr 구현

---
+++ src/main/java/racingcar/controller/GameController.java
@@ -0,0 +1,43 @@
+package racingcar.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class GameController {
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+
+    public void run() {
+        Cars cars = setCars();
+        int tryCount = setTryCount();
+        gameStart(tryCount, cars);
+    }
+
+    public void gameStart(int tryCount, Cars cars) {
+        outputView.printResultStart();
+        for(int i=0; i<tryCount; i++) {
+            cars.moveCar();
+            outputView.printResultTurn(cars.getResult());
+        }
+        outputView.printWinner(cars.getWinner());
+    }
+
+    public int setTryCount() {
+        outputView.printRequestTryCount();
+        int tryCount = inputView.inputTryCount();
+        return tryCount;
+    }
+
+
+    public Cars setCars() {
+        outputView.printRequestName();
+        List<String> carNames = inputView.inputCars();
+        List<Car> carList = carNames.stream().map(name -> new Car(name)).collect(Collectors.toList());
+        Cars cars = new Cars(carList);
+        return cars;
+    }
+}
