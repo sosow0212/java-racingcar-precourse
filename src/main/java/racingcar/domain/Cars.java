3008231758d125209ce3a680a6e4ba9e98f8d664
sosow0212 <sosow0212@naver.com>
2022-12-01 14:21:54 +0900

    feat : (Cars) 이동 값 가장 높은 자동차를 반환해주기 위한 메서드 구현

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -5,16 +5,14 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class Game {
-    private final int gameTryCount;
+public class Cars {
     private final List<Car> cars;
 
-    public Game(int gameTryCount, List<Car> cars) {
-        this.gameTryCount = gameTryCount;
+    public Cars(List<Car> cars) {
         this.cars = cars;
     }
 
-    public void run() {
+    public void moveCar() {
         cars.forEach(car -> car.move());
     }
 
@@ -22,5 +20,17 @@ public class Game {
         HashMap<String, Integer> result = new HashMap<>();
         cars.stream().map(i -> result.put(i.getName(), i.getPosition())).collect(Collectors.toList());
         return result;
+    }
+
+    public List<String> getWinner() {
+        List<String> winner = cars.stream()
+                .filter(i -> getMaxIndex() == i.getPosition())
+                .map(i -> i.getName())
+                .collect(Collectors.toList());
+        return winner;
+    }
+
+    public int getMaxIndex() {
+        return cars.stream().mapToInt(i -> i.getPosition()).max().orElse(0);
     }
 }
